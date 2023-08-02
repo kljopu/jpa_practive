26b5dcc11e7a90f4702ae6cc67eb0b945897ac30
kljopu <kljopuu@gmail.com>
2023-08-02 23:05:14 +0900

    6강 순수 JPA 기반 Repo 생성

--- src/main/java/jpastudy/datajpa/repotitory/MemberJPARepository.java
+++ src/main/java/jpastudy/datajpa/repotitory/MemberJPARepository.java
@@ -2,6 +2,8 @@ package jpastudy.datajpa.repotitory;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 import jpastudy.datajpa.entity.Member;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +20,24 @@ public class MemberJPARepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member  m", Long.class).getSingleResult();
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        //JPQL
+        return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 
 }
