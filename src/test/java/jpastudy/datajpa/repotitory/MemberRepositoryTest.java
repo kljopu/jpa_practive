6dcac9e434c325c57fb476fedbeace18b0b61a0d
kljopu <kljopuu@gmail.com>
2023-08-03 12:48:21 +0900

    common repositoru interface

--- src/test/java/jpastudy/datajpa/repotitory/MemberRepositoryTest.java
+++ src/test/java/jpastudy/datajpa/repotitory/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpastudy.datajpa.repotitory;
 
+import java.util.List;
 import java.util.Optional;
 import jpastudy.datajpa.entity.Member;
 import org.assertj.core.api.Assertions;
@@ -29,5 +30,35 @@ class MemberRepositoryTest {
         assertThat(savedMember.getId()).isEqualTo(foundMember.getId());
         assertThat(savedMember.getUsername()).isEqualTo(foundMember.getUsername());
 
+    }
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회 검증
+        Member foundMember1 = memberRepository.findById(member1.getId()).get();
+        Member foundMember2 = memberRepository.findById(member1.getId()).get();
+
+        assertThat(foundMember1).isEqualTo(foundMember2);
+        assertThat(foundMember2).isEqualTo(foundMember1);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
